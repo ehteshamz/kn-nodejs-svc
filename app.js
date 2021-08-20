af9a130b46d9e9ede7d9390b3b404c69e81ba9b1
Peter Mbanugo <peter@foobar.agency>
2021-08-20 23:27:06 +0200

    init app

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = require("express")()
+const server = require("http").createServer(app)
+const port = process.env.PORT || "8080"
+const message = process.env.TARGET || 'Hello World'
+
+app.get('/', (req, res) => res.send(message))
+server.listen(port, function () {
+    console.log(`App listening on ${port}`)
+});
+
