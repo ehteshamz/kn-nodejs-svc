ca99d3b0023fbb9a3f16531954e20fcec2c19c06
ehteshamz <ehtesham.zahoor@educative.io>
2022-08-03 09:20:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const port = process.env.PORT || "8080";
-const message = "Boonjoour " + process.env.TARGET || "World!!";
+const message = "Ca va? " + process.env.TARGET || "World!!";
 
 app.get("/", (req, res) => res.send(message));
 server.listen(port, function () {
